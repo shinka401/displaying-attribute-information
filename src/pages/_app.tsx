9fa092fbce6f23cc37d2aae358751d583be747eb
shinka401 <shinka401unauthorized@gmail.com>
2023-02-20 23:15:35 +0900

    折線グラフ出すだけならいい感じ

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,6 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
-import Chart from "chart.js/auto";
-import "chartjs-adapter-date-fns";
-import ChartStreaming from "chartjs-plugin-streaming";
-Chart.register(ChartStreaming);
-
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
